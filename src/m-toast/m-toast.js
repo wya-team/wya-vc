5de38e23961d1f94bcd2f6b522a10835868a4e80
Mr.Z <zrd0921@qq.com>
2018-12-19 17:49:57 +0800

    add:m-toast

--- src/m-toast/m-toast.js
+++ src/m-toast/m-toast.js
@@ -1,14 +1,16 @@
 import Vue from 'vue';
 import MToast from './m-toast.vue';
 import { VcInstance } from '../vc/index';
+import { getUid } from '../utils/index';
 
 const Dom = document.body;
-const cName = "MToast";
+const basicName = "vcm-toast";
 
 const Target = {
-	info(message, duration = 3, onCallback, showClose = true, opts = {}) {
+	vm: [],
+	init(message, duration = 3, callback, maskClosable = true, opts = {}) {
 
-		VcInstance.APIS[cName] && VcInstance.APIS[cName].$emit('close');
+		let cName = `${basicName}-${getUid()}`;
 
 		const div = document.createElement('div');
 		const VueComponent = Vue.extend(MToast);
@@ -19,28 +21,78 @@ const Target = {
 				...opts,
 				message,
 				duration: duration == 0 ? 1 * 60 * 60 * 24 : duration,
-				showClose
+				maskClosable
 			},
 			methods: {
 			}
 		});
 
-		vm.$on('callback', () => {
-			onCallback && onCallback();
+		vm.$on('close', () => {
+			vm.$emit('destroy');
+			callback && callback();
 		});
 		
-		vm.$on('close', () => {
+		vm.$on('destroy', () => {
 			vm.$destroy();
 			// 主动卸载节点
 			Dom.removeChild(vm.$el);
 			delete VcInstance.APIS[cName];
 		});
 
+		// 不自动销毁
+		vm.__AUTO_DESTORY__ = false;
+
 		VcInstance.APIS[cName] = vm;
 
 		document.body.appendChild(vm.$el);
 
 		return vm;
+	},
+	info(...params) {
+		params[4] = {
+			mode: 'info'
+		};
+		return this.init(...params);
+	},
+	loading(...params) {
+		params[4] = {
+			mode: 'loading'
+		};
+		return this.init(...params);
+	},
+	success(...params) {
+		params[4] = {
+			mode: 'success'
+		};
+		return this.init(...params);
+	},
+	warn(...params) {
+		params[4] = {
+			mode: 'warn'
+		};
+		return this.init(...params);
+	},
+	error(...params) {
+		params[4] = {
+			mode: 'error'
+		};
+		return this.init(...params);
+	},
+	hide(id) {
+		try {
+			if (id && VcInstance.APIS[id]) {
+				VcInstance.APIS[id];
+			} else {
+				Object.keys(VcInstance.APIS).forEach(item => {
+					if (item.includes(basicName)) {
+						VcInstance.APIS[item] && VcInstance.APIS[item].$emit('destory');
+					}
+				});
+			}
+		} catch (e) {
+			console.log('vcm-toast', e);
+		}
+		
 	}
 };
 
