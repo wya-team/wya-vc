5993f3e89efd567e9910b062e31cae6a2cb8c09e
JDEast1029 <852778359@qq.com>
2019-01-17 20:04:42 +0800

    add: cascader init

---
+++ src/cascader/cascader.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>cascader</div>
+</template>
+<script>
+export default {
+	name: "vc-cascader",
+	props: {
+	},
+	data() {
+		return {
+		};
+	},
+	computed: {
+		
+	},
+	methods: {
+	}
+};
+</script>
+<style></style>
---
+++ src/cascader/examples/basic.vue
@@ -0,0 +1,22 @@
+<template>
+	<vc-cascader />
+</template>
+<script>
+import Cascader from '../cascader';
+
+export default {
+	name: "vc-cascader-basic",
+	components: {
+		"vc-cascader": Cascader
+	},
+	data() {
+		return {
+		};
+	},
+	computed: {
+		
+	},
+	methods: {
+	}
+};
+</script>
