070af13886a0012bce885dd4571f0a90883df294
mario <18758191423@163.com>
2019-01-22 15:27:00 +0800

    add: progress

---
+++ src/progress/examples/basic.vue
@@ -0,0 +1,48 @@
+<template>
+	<div>
+		<div>
+			<vc-progress
+				:stroke-width="7"
+				:percent="50"
+				status="active"
+			/>
+		</div>
+		<div>
+			<vc-progress
+				:stroke-width="7"
+				:percent="50"
+				status="error"
+			/>
+		</div>
+		<div>
+			<vc-progress
+				:stroke-width="7"
+				:percent="100"
+			/>
+		</div>
+		<vc-progress
+			:stroke-width="5" 
+			:percent="70"
+			type="circle"
+		/>
+	</div>
+</template>
+<script>
+import Progress from '../progress';
+
+export default {
+	name: "vc-tpl-basic",
+	components: {
+		'vc-progress': Progress
+	},
+	data() {
+		return {
+		};
+	},
+	computed: {
+		
+	},
+	methods: {
+	}
+};
+</script>
---
+++ src/progress/progress.vue
@@ -0,0 +1,252 @@
+<template>	
+	<div class="vc-progress">
+		<div v-if="type === 'line'" :class="currentStatus" class="__line">
+			<div class="__wrapper">
+				<div class="__box round">
+					<div 
+						:style="innerStyle"
+						class="__inner"
+					/>
+				</div>
+			</div>
+			<div v-if="showInfo" class="__percent">
+				<vc-icon v-if="isStatus" :type="currentStatus"/>
+				<span v-else>{{ percent }}%</span>
+			</div>
+		</div>
+		<div v-else class="__circle">
+			<div :style="circleBoxSize" class="__box">
+				<svg viewBox="0 0 100 100">
+					<path 
+						:d="pathString"
+						:stroke-width="strokeWidth" 
+						stroke="#eaeef2" 
+						fill-opacity="0"
+					/>
+					<path 
+						:d="pathString"
+						:style="pathStyle"
+						:stroke-width="strokeWidth" 
+						stroke-linecap="round" 
+						stroke="#2d8cf0" 
+						fill-opacity="0" 
+					/>
+				</svg>
+				<div class="__inner">
+					<slot>
+						<span>{{ percent }}%</span>
+					</slot>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+import Icon from '../icon';
+
+export default {
+	name: "vc-progress",
+	components: {
+		'vc-icon': Icon
+	},
+	props: {
+		type: {
+			type: String,
+			default: 'line'
+		},
+		percent: {
+			type: Number,
+			default: 0
+		},
+		status: {
+			validator(val) {
+				return ['normal', 'active', 'error', 'success'].includes(val);
+			},
+			default: 'normal'
+		},
+		showInfo: {
+			type: Boolean,
+			default: true
+		},
+		strokeWidth: {
+			type: Number,
+			default: 10
+		},
+		size: {
+			type: Number,
+			default: 120
+		}
+	},
+	data() {
+		return {
+			currentStatus: this.status
+		};
+	},
+	computed: {
+		circleBoxSize() {
+			return {
+				width: `${this.size}px`, 
+				height: `${this.size}px`,
+				position: 'relative'
+			};
+		},
+		innerStyle() {
+			let style = {
+				height: this.strokeWidth + 'px',
+				width: this.percent + '%'
+			};
+			return style;
+		},
+		isStatus() {
+			return this.currentStatus === 'error' || this.currentStatus === 'success';
+		},
+		radius() {
+			return 50 - this.strokeWidth / 2;
+		},
+		len() {
+			return Math.PI * 2 * this.radius;
+		},
+		pathString() {
+			return `M 50,50 m 0,-${this.radius}
+			a ${this.radius},${this.radius} 0 1 1 0,${2 * this.radius}
+			a ${this.radius},${this.radius} 0 1 1 0,-${2 * this.radius}`;
+		}, // path路劲画圆
+		pathStyle() {
+			let style = {};
+			style = {
+				'stroke-dasharray': `${this.len}px ${this.len}px`,
+				'stroke-dashoffset': `${((100 - this.percent) / 100 * this.len)}px`,
+				'transition': 'stroke-dashoffset 0.6s ease 0s, stroke 0.6s ease'
+			};
+			return style;
+		}
+	},
+	watch: {
+		percent(val) {
+			console.log(val);
+		},
+		status(val) {
+			this.currentStatus = val;
+		}
+	},
+	created() {
+		this.handleStatus();
+	},
+	methods: {
+		handleStatus() {
+			if (parseInt(this.percent, 10) == 100) {
+				this.currentStatus = 'success';
+			}
+		}
+	}
+};
+</script>
+<style lang="less" scoped>
+.vc-progress{
+	.__line{
+		.__wrapper{
+			width: 100%;
+			margin-right: -30px;
+			padding-right: 33px;
+			display: inline-block;
+			vertical-align: middle;
+			.__box{
+				width: 100%;
+				background-color: #f3f3f3;
+				overflow: hidden;
+				&.round{
+					border-radius: 8px;	
+				}
+				.__inner{
+					position: relative;
+					transition: width .5s cubic-bezier(.075,.82,.165,1);
+					border-radius: 8px;
+					background-color: #1890ff; 
+				}
+			}
+		}
+		.__percent{
+			display: inline-block;
+			vertical-align: middle;
+		}
+		&.success{
+			.__wrapper{
+				.__box{
+					.__inner{
+						background-color: #19be6b;
+					}
+				}
+			}
+			.__percent{
+				.wyaicon{
+					font-size: 12px;
+					color: #19be6b;
+				}
+			}
+		}
+		&.error{
+			.__wrapper{
+				.__box{
+					.__inner{
+						background-color: #ed4014;
+					}
+				}
+			}
+			.__percent{
+				.wyaicon{
+					font-size: 12px;
+					color: #ed4014;
+				}
+			}
+		}
+		&.active{
+			.__wrapper{
+				.__box{
+					.__inner{
+						&::before{
+							content: '';
+							position: absolute;
+							left: 0;
+							top: 0;
+							height: 100%;
+							background-color: #fff;
+							animation: handleWidth 2s infinite;
+							border-radius: 8px;
+						}
+					}
+				}
+			}
+		}
+	}
+	.__circle{
+		.__box{
+			width: 120px;
+			height: 120px;
+			.__inner{
+				position: absolute;
+				top: 50%;
+				left: 50%;
+				transform: translate(-50%, -50%);
+			}
+		}
+	}
+
+@keyframes handleWidth{
+	0%{
+		width: 0;
+		opacity: 0;
+	}
+	40%{
+		opacity: .2;
+	}
+	80%{
+		width: 100%;
+		opacity: 0;
+	}
+	100%{
+		width: 100%;
+		opacity: 0;
+	}
+}
+}
+</style>
