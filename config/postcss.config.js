eec18e8530622480a897e6d63cebbdeabb24ce50
Mr.Z <zrd0921@qq.com>
2019-03-07 21:47:19 +0800

    update: autoprefixer

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -12,7 +12,16 @@ module.exports = {
 		require('autoprefixer')({
 			/* ...options */
 			// 不删除老式写法
-			remove: false
+			remove: false,
+			// 样式前缀添加
+			browsers: [
+				"> 1%", 
+				"IE > 8",
+				"last 2 versions", 
+				"Firefox ESR", 
+				"Opera 12.1",
+				"Safari > 7"
+			]
 		})
 	]
 };
