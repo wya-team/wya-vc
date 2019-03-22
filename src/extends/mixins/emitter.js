1ad7546cb1f92c857c77c551bb13c27c158a1d3a
Mr.Z <zrd0921@qq.com>
2019-03-22 23:48:50 +0800

    fix:emitter

--- src/extends/mixins/emitter.js
+++ src/extends/mixins/emitter.js
@@ -3,12 +3,12 @@ export default {
 		dispatch(componentName, eventName, params) {
 			let parent = this.$parent || this.$root;
 			let name = parent.$options.name;
-
+			
 			while (parent && (!name || name !== componentName)) {
 				parent = parent.$parent;
 
 				if (parent) {
-					name = parent.$options.componentName;
+					name = parent.$options.name;
 				}
 			}
 			if (parent) {
