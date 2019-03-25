a88e7b1c1f886323a7106d895cd18504b490a8b7
Mr.Z <zrd0921@qq.com>
2019-03-25 11:33:11 +0800

    update:m-toast with portal

--- src/m-toast/m-toast.js
+++ src/m-toast/m-toast.js
@@ -2,103 +2,61 @@ import Vue from 'vue';
 import MToast from './m-toast.vue';
 import { VcInstance } from '../vc/index';
 import { getUid, getOption } from '../utils/index';
+import CreatePortal from '../create-portal/index';
 
-const Dom = document.body;
-const basicName = "vcm-toast";
+const registerOptions = {
+	multiple: true,
+	promise: false, 
+	autoDestroy: false
+};
 
-const Target = {
-	init(opts = {}) {
-		let cName = `${basicName}-${getUid()}`;
+class MToastManager extends CreatePortal.Core {
+	constructor(globalOptions, wrapper) { // eslint-disable-line
+		super(globalOptions, wrapper);
+		// todo
+	}
 
-		const div = document.createElement('div');
-		const VueComponent = Vue.extend(MToast);
-		let vm;
-		vm = new VueComponent({
-			el: div,
-			propsData: {
-				...opts
-			},
-			methods: {
-			}
-		});
-
-		vm.$on('close', () => {
-			vm.$emit('destroy');
-			opts.onClose && opts.onClose();
-		});
-		
-		vm.$on('destroy', () => {
-			vm.$destroy();
-			// 主动卸载节点
-			Dom.removeChild(vm.$el);
-			delete VcInstance.APIS[cName];
-
-			vm = null;
-		});
-
-		// 不自动销毁
-		vm.__AUTO_DESTORY__ = false;
-
-		VcInstance.APIS[cName] = vm;
-
-		document.body.appendChild(vm.$el);
-
-		return vm;
-	},
 	run(params, opts) {
 		let query = ['content', 'duration', 'onClose', 'maskClosable'];
-		let result = {
+		let options = {
 			...opts,
 			...getOption(params, query)
 		};
 
 		// 执行弹窗
-		this.init(result);
-	},
+		return this.popup(options);
+	}
+
 	info(...params) {
 		this.run(params, {
 			mode: 'info'
 		});
-	},
+	}
+
 	loading(...params) {
 		this.run(params, {
 			mode: 'loading',
 			duration: 0,
 			maskClosable: false
 		});
-	},
+	}
+
 	success(...params) {
 		this.run(params, {
 			mode: 'success'
 		});
-	},
+	}
+
 	warn(...params) {
 		this.run(params, {
 			mode: 'warn'
 		});
-	},
+	}
+
 	error(...params) {
 		this.run(params, {
 			mode: 'error'
 		});
-	},
-	destroy(id) {
-		try {
-			if (id) {
-				let instance = typeof id === 'object' ? id : VcInstance.APIS[id];
-				instance && instance.$emit('destroy');
-			} else {
-				Object.keys(VcInstance.APIS).forEach(item => {
-					if (item.includes(basicName)) {
-						VcInstance.APIS[item] && VcInstance.APIS[item].$emit('destroy');
-					}
-				});
-			}
-		} catch (e) {
-			console.log('vcm-toast', e);
-		}
-		
 	}
-};
-
-export default Target;
+}
+export default new MToastManager(registerOptions, MToast);
