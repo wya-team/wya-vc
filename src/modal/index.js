08107e6e92fcdf675edabe09b725d172020c1ebb
mario <18758191423@163.com>
2019-03-25 15:29:42 +0800

    update sucess top success

--- src/modal/index.js
+++ src/modal/index.js
@@ -2,7 +2,7 @@ import { createModal } from './confirm';
 import Modal from './modal';
 
 Modal.info = createModal('info');
-Modal.success = createModal('sucess');
+Modal.success = createModal('success');
 Modal.error = createModal('error');
 Modal.warning = createModal('warning');
 
