9665f37341758b581ce49c23504aaaff3368b1cb
Mr.Z <zrd0921@qq.com>
2019-03-25 11:31:15 +0800

    update: create-portal

---
+++ src/create-portal/core.js
@@ -0,0 +1,249 @@
+import Vue from 'vue';
+import { cloneDeep, getUid, eleInRegExp } from '../utils/index';
+import { VcBasic, VcError } from '../vc/index';
+import defaultOptions from './default-options';
+
+class PortalCore extends VcBasic {
+	constructor(registerOptions = {}, wrapper) {
+		super();
+
+		let { cName, ...globalOptions } = registerOptions;
+
+
+		if (!wrapper) {
+			throw new VcError('create-portal', '目标组件必传');
+		}
+		if (!cName && !wrapper.name) {
+			throw new VcError('create-portal', 'cName 必传');
+		}
+
+		cName = cName || wrapper.name;
+
+		this.waiting = false;
+
+		this.wrapper = wrapper;
+
+		// 与全局配置, 重新生成默认配置
+		this.defaultOptions = {
+			...defaultOptions,
+			store: this.config.store,
+			router: this.config.router,
+			...this.config.CreatePortal, 
+			...globalOptions,
+			cName
+		};
+	}
+
+	/**
+	 * 弹出项目，验证数据结构是否合法
+	 * userOptions {
+	 * 	store,
+	 * 	router,
+	 * 	getInstance,
+	 * 	param: {
+	 * 	}
+	 * }
+	 */
+	popup(userOptions = {}) {
+		if (typeof userOptions !== 'object') {
+			userOptions = {};
+		}
+		return this._init({ ...this.defaultOptions, ...userOptions });
+	}
+
+	_init(options) {
+		const { onBefore, onSure, onClose, promise, ...rest } = options;
+
+		return promise 
+			? new Promise((resolve, reject) => {
+				if (onBefore && !this.waiting) {
+					this.waiting = true;
+					onBefore({ ...options })
+						.then((response = {}) => {
+							this._render({ 
+								options, 
+								response,
+								onSure: resolve,
+								onClose: reject,
+							});
+						}).catch((res = {}) => {
+							this.waiting = false;
+							reject(res);
+						});
+				} else {
+					this._render({ 
+						options, 
+						onSure: resolve,
+						onClose: reject,
+					});
+				}
+			})
+			: this._render({ 
+				options, 
+				onSure, 
+				onClose 
+			});
+	}
+
+	_createCallback(vm, options, callback) {
+
+		const { leaveDelay } = options;
+
+		return (res, opts = {}) => {
+			
+			setTimeout(() => vm && vm.$emit('destroy'), leaveDelay * 1000);
+
+			callback && callback(res);
+		};
+	}
+
+	_render({ options, response = {}, onSure, onClose }) {
+		let { 
+			el, 
+			tag, 
+			alive, // 再次调用，实例不销毁
+			aliveRegExp, // 实例以外且该数组内的, 点击不销毁
+			aliveKey,
+			cName,
+			leaveDelay,
+			autoDestroy,
+			getInstance, 
+			onBefore, 
+			store,
+			router,
+			parent,
+			data,
+			components,
+			multiple,
+			slots,
+			...rest
+		} = options;
+
+		cName = multiple ? `${cName}__${getUid()}` : cName;
+
+		let container = document.createElement(tag);
+		let target = typeof el === 'string' ? document.querySelector(el || 'body') : el;
+		
+		// destory
+		!alive && this.APIS[cName] && this.APIS[cName].$emit('destroy');
+
+		let propsData = {
+			...rest,
+			data: response.data || data
+		};
+
+		// vm
+		let vm;
+		if (alive && this.APIS[cName]) {
+
+			vm = this.APIS[cName];
+			vm.$off(['destroy', 'close', 'sure']);
+
+			for (let key in propsData) {
+				vm[key] = propsData[key];
+			}
+
+			// update
+			let fn = vm.update || vm.loadData;
+			fn && fn(options);
+			
+		} else {
+			const wrapper = cloneDeep(this.wrapper);
+			const VueComponent = Vue.extend(wrapper);
+			
+			vm = new VueComponent({
+				el: container,
+				store, // vuex,
+				router, // vue-router
+				propsData,
+				components: { 
+					...wrapper.components, 
+					...components 
+				},
+				...parent,
+				mounted() {
+					alive && document.addEventListener('click', this.handleExtra, true);
+				},
+				destroyed() {
+					
+					alive && document.removeEventListener('click', this.handleExtra, true);
+				},
+				methods: {
+					handleExtra(e) {
+						// close默认不传，用户可传递参数判断输入自己的触发的close
+						try {
+							let path = e.path || (e.composedPath && e.composedPath()) || [];
+							if (
+								!this.$el.contains(e.target) 
+								&& !path.some(item => eleInRegExp(item, aliveRegExp))
+							) {
+								if (this.$children[0] && this.$children[0][aliveKey]) {
+									this.$children[0][aliveKey] = false;
+									setTimeout(() => this.$emit('close'), leaveDelay * 1000);
+								} else {
+									this.$emit('close');
+								}
+							}
+						} catch (e) {
+							console.log(e);
+						}
+						
+					}
+				}
+			});
+		}
+
+		// slots 2.6.0+ 的slotx写法会同步更新
+		vm.$slots = slots;
+
+		// tag
+		vm.__AUTO_DESTROY__ = autoDestroy;
+
+		vm.$on('destroy', (res) => {
+			vm.$destroy();
+			// 主动卸载节点
+			target.removeChild(vm.$el);
+			delete this.APIS[cName];
+
+			vm = null;
+		});
+
+		vm.$on('close', this._createCallback(vm, options, onClose));
+
+		vm.$on('sure', this._createCallback(vm, options, onSure));
+
+		// 回调vm实例
+		getInstance && getInstance(vm);
+
+		// 标记
+		this.APIS[cName] = vm;
+
+		// 插入	
+		vm.$el.parentElement === null && target.appendChild(vm.$el);
+
+		this.waiting = false;
+
+		return vm;
+	}
+	
+	destroy(target) {
+		const { multiple, cName } = this.defaultOptions;
+		target = target || cName;
+		const instance = typeof target === 'object' 
+			? target 
+			: this.APIS[target];
+
+		if (instance) {
+			instance.$emit('destroy');
+		} else if (multiple) {
+			Object.keys(this.APIS).forEach(item => {
+				if (item.includes(cName)) {
+					console.log(item);
+					this.APIS[item] && this.APIS[item].$emit('destroy');
+				}
+			});
+		}
+	}
+}
+
+export default PortalCore;
---
+++ src/create-portal/default-options.js
@@ -0,0 +1,18 @@
+export default {
+	tag: 'div', 
+	el: 'body', 
+	cName: null,
+	alive: false, // 再次调用，实例不销毁
+	multiple: false, // 多个实例共存
+	promise: true,
+	aliveRegExp: { className: /(vc-hack-alive|vc-hack-cp)/ },
+	aliveKey: 'visible',
+	leaveDelay: 0.3,
+	autoDestroy: true,
+	getInstance: null, 
+	onBefore: null, 
+	parent: {}, // 依赖注入使用 like store, router, Life cycle，methods, mixins, ....
+	components: {}, // 可以动态注入组件
+	slots: {},
+	data: null,
+};
