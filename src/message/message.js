db95a6d2ef6f5c00a4d77fc962048fe0d67f012d
Mr.Z <zrd0921@qq.com>
2019-03-25 11:49:40 +0800

    update: message with portal

--- src/message/message.js
+++ src/message/message.js
@@ -1,103 +1,63 @@
 import Vue from 'vue';
 import Message from './message.vue';
-import { VcInstance } from '../vc/index';
-import { getUid, getOption } from '../utils/index';
+import { getOption } from '../utils/index';
+import CreatePortal from '../create-portal/index';
 
-const Dom = document.body;
-const basicName = "vc-message";
+const registerOptions = {
+	multiple: true,
+	promise: false, 
+	autoDestroy: false
+};
 
-const Target = {
-	init(opts = {}) {
-		let cName = `${basicName}-${getUid()}`;
-		const div = document.createElement('div');
-		const VueComponent = Vue.extend(Message);
-		let vm;
-		let number = Object.keys(VcInstance.APIS).filter(item => item.includes(basicName)).length;
-		vm = new VueComponent({
-			el: div,
-			propsData: {
-				...opts,
-				top: 30 + number * 40,
-			},
-			methods: {
-			}
-		});
+class MessageManager extends CreatePortal.Core {
+	constructor(globalOptions, wrapper) { // eslint-disable-line
+		super(globalOptions, wrapper);
+		// todo
+	}
 
-		vm.$on('close', () => {
-			vm.$emit('destroy');
-			opts.onClose && opts.onClose();
-		});
-		
-		vm.$on('destroy', () => {
-			vm.$destroy();
-			// 主动卸载节点
-			Dom.removeChild(vm.$el);
-			delete VcInstance.APIS[cName];
-
-			vm = null;
-		});
-
-		// 不自动销毁
-		vm.__AUTO_DESTROY__ = false;
-
-		VcInstance.APIS[cName] = vm;
-
-		document.body.appendChild(vm.$el);
-
-		return vm;
-	},
 	run(params, opts) {
 		let query = ['content', 'duration', 'onClose'];
-		let result = {
+		let number = Object.keys(this.APIS)
+			.filter(item => item.includes(this.defaultOptions.cName)).length;
+		let options = {
 			...opts,
 			...getOption(params, query),
+			top: 30 + number * 40,
 		};
 		// 执行弹窗
-		this.init(result);
-	},
+		return this.popup(options);
+	}
+
 	info(...params) {
 		this.run(params, {
 			mode: 'info'
 		});
-	},
+	}
+
 	loading(...params) {
 		this.run(params, {
 			mode: 'loading',
 			duration: 0,
 			maskClosable: false
 		});
-	},
+	}
+
 	success(...params) {
 		this.run(params, {
 			mode: 'success'
 		});
-	},
+	}
+
 	warn(...params) {
 		this.run(params, {
 			mode: 'warn'
 		});
-	},
+	}
+
 	error(...params) {
 		this.run(params, {
 			mode: 'error'
 		});
-	},
-	destroy(id) {
-		try {
-			if (id) {
-				let instance = typeof id === 'object' ? id : VcInstance.APIS[id];
-				instance && instance.$emit('destroy');
-			} else {
-				Object.keys(VcInstance.APIS).forEach(item => {
-					if (item.includes(basicName)) {
-						VcInstance.APIS[item] && VcInstance.APIS[item].$emit('destroy');
-					}
-				});
-			}
-		} catch (e) {
-			console.log('vc-message', e);
-		}
 	}
-};
-
-export default Target;
+}
+export default new MessageManager(registerOptions, Message);
