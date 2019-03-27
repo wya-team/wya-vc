44a6f648dd2dc516908d3f094dae84c7e15c2cb4
Mr.Z <zrd0921@qq.com>
2019-03-28 07:50:11 +0800

    update:icon event

--- src/icon/icon.vue
+++ src/icon/icon.vue
@@ -1,5 +1,5 @@
 <template>
-	<svg class="vc-icon" aria-hidden="true">
+	<svg class="vc-icon" aria-hidden="true" @click="$emit('click', $event)">
 		<use :xlink:href="`#icon-${type}`"/>
 	</svg>
 </template>
