1aaf51edbae14f7f6d627adea0a29b5fdba13254
Mr.Z <zrd0921@qq.com>
2019-03-29 19:20:00 +0800

    update: icon demo

--- src/icon/examples/basic.vue
+++ src/icon/examples/basic.vue
@@ -1,16 +1,28 @@
 <template>
-	<vc-icon type="up" />
+	<div>
+		<h1>点击复制</h1>
+		<div class="vc-icon-basic">
+			<span v-for="item in items" :key="item" >
+				<vc-icon :type="item"/>
+				<vc-copy :value="`<vc-icon type=&quot;${item}&quot; />`">{{ item }}</vc-copy>
+			</span>
+		</div>
+	</div>
 </template>
 <script>
 import Icon from '../icon';
+import Copy from '../../copy';
+import all from '../svg';
 
 export default {
 	name: "vc-icon-basic",
 	components: {
-		'vc-icon': Icon
+		'vc-icon': Icon,
+		'vc-copy': Copy
 	},
 	data() {
 		return {
+			items: all.match(/id="icon-(\S{1,})"/g).map(i => i.replace(/("|id="icon-)/g, ""))
 		};
 	},
 	computed: {
@@ -20,3 +32,17 @@ export default {
 	}
 };
 </script>
+<style>
+.vc-icon-basic {
+	display: flex;
+	flex-wrap: wrap;
+	span {
+		width: 200px;
+		padding: 20px;
+		text-align: center;
+	}
+	i {
+		font-size: 30px
+	}
+}
+</style>
