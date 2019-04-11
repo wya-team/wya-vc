2b475937d772b06fd7631e86046f3cbe928b5e26
Mr.Z <zrd0921@qq.com>
2019-04-11 20:32:25 +0800

    update:expand with vc-transition

--- src/expand/expand.vue
+++ src/expand/expand.vue
@@ -1,102 +1,52 @@
 <template>
-	<transition
-		v-show="isActive" 
-		@before-enter="beforeEnter" 
-		@enter="enter" 
-		@after-enter="afterEnter" 
-		@before-leave="beforeLeave" 
-		@leave="leave" 
-		@after-leave="afterLeave"
-	>
+	<vc-transition-collapse>
 		<component v-show="isActive" :is="tag">
 			<slot />
 		</component>
-	</transition>
+	</vc-transition-collapse>
 </template>
 
 <script>
 import { VcInstance } from '../vc/index';
+import Transition from '../transition/index';
 
 export default {
 	name: "vc-expand",
+	components: {
+		'vc-transition-collapse': Transition.Collapse
+	},
 	model: {
-		prop: 'show',
-		event: 'change'
+		prop: 'visible',
+		event: 'visible-change'
 	},
 	props: {
 		tag: {
 			type: String,
 			default: 'div'
 		},
-		show: {
+		visible: {
 			type: Boolean,
 			default: false
 		}
 	},
 	data() {
 		return {
-			isActive: this.show
+			isActive: this.visible
 		};
 	},
 	computed: {
 		
 	},
 	methods: {
-		// 需要额外调整，使用transition中的name
-		beforeEnter(el) {
-			el.className += ' vc-expand';
-			 if (!el.dataset) el.dataset = {};
-
-			el.style.height = '0';
-		},
-		enter(el) {
-			el.dataset.oldOverflow = el.style.overflow;
-			if (el.scrollHeight !== 0) {
-				el.style.height = el.scrollHeight + 'px';
-			} else {
-				el.style.height = '';
-			}
-
-			el.style.overflow = 'hidden';
-		},
-		afterEnter(el) {
-			el.className = el.className.replace(' vc-expand', '');
-			el.style.height = '';
-			el.style.overflow = el.dataset.oldOverflow;
-		},
-		beforeLeave(el) {
-			if (!el.dataset) el.dataset = {};
-			el.dataset.oldOverflow = el.style.overflow;
-
-			el.style.height = el.scrollHeight + 'px';
-			el.style.overflow = 'hidden';
-		},
-		leave(el) {
-			if (el.scrollHeight !== 0) {
-				el.className += ' vc-expand';
-				el.style.height = 0;
-			}
-		},
-		afterLeave(el) {
-			el.className = el.className.replace(' vc-expand', '');
-			el.style.height = '';
-			el.style.overflow = el.dataset.oldOverflow;
-		},
-
 		/**
 		 * 外部调用
 		 */
 		toggle() {
 			this.isActive = !this.isActive;
-			this.$emit('change', this.isActive);
+			this.$emit('visible-change', this.isActive);
 
-			VcInstance.emit('vc-expand', { show: this.isActive });
+			VcInstance.emit('vc-expand', { visible: this.isActive });
 		}
 	}
 };
 </script>
-<style>
-.vc-expand {
-	transition: height .2s ease-in-out
-}
-</style>
