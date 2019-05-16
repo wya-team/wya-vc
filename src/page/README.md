b0b0e6394db627ac65c771046ac9c0259088a5d7
Mr.Z <zrd0921@qq.com>
2019-05-16 09:28:06 +0800

    update:page

--- src/page/README.md
+++ src/page/README.md
@@ -9,20 +9,15 @@
 属性 | 说明 | 类型 | 默认值
 ---|---|---|---
 current | 当前页码，支持 .sync 修饰符 | Number | 1
-total | 数据总数 | Number | 0
+total | 数据总页数 | Number | 0
+count | 数据总数 | Number | 0
 page-size | 每页条数 | Number | 10
 page-size-opts | 每页条数切换的配置 | Array | [10, 20, 30, 40]
 placement | 条数切换弹窗的展开方向，可选值为 `bottom` 和 `top` | String | bottom
-size | 可选值为`small`（迷你版）或不填（默认） | String | -
-simple | 简洁版 | Boolean | false
-show-total | 显示总数 | Boolean | false
+show-count | 显示总数 | Boolean | false
 show-elevator | 显示电梯，可以快速切换到某一页 | Boolean | false
 show-sizer | 显示分页，用来改变`page-size` | Boolean | false
-class-name | 自定义 class 名称 | String | -
-styles | 自定义 style 样式 | Object | -
 portal | 是否将弹层放置于 body 内，在 Tabs、带有 fixed 的 Table 列内使用时，建议添加此属性，它将不受父级样式影响，从而达到更好的效果 | Boolean | true
-prev-text | 替代图标显示的上一页文字 | String | -
-next-text | 替代图标显示的下一页文字 | String | -
 
 #### 事件
 
--- src/page/page.vue
+++ src/page/page.vue
@@ -1,59 +1,350 @@
 <template>
-	<i-page 
-		:current="current"
-		:page-size="pageSize"
-		:page-size-opts="pageSizeOpts"
-		:placement="placement"
-		:size="size"
-		:total="count || total"
-		:simple="simple"
-		:show-total="showCount || showTotal"
-		:show-elevator="showElevator"
-		:show-sizer="showSizer"
-		:class-name="className"
-		:styles="styles"
-		:transfer="portal"
-		:prev-text="prevText"
-		:next-text="nextText"
-		@on-change="$emit('change', arguments[0])"
-		@on-page-size-change="$emit('page-size-change', arguments[0])"
-		@update:current="$emit('update:current', arguments[0])"
-	>
-		<slot />
-	</i-page>
+	<div class="vc-page">
+		<span v-if="showCount" class="vc-page__count">
+			<slot>
+				<span>共 {{ count }} 条</span> 
+			</slot>
+		</span>
+		<div
+			:class="{'is-disabled': currentPage == 1 }"
+			title="prev"
+			class="vc-page__item is-icon"
+			@click="handlePrev"
+		>
+			<vc-icon type="left" />
+		</div>
+		<!-- 第一页 -->
+		<div 
+			:class="{'is-active': currentPage == 1 }"
+			class="vc-page__item"
+			title="1"
+			@click="handleChangePage(1)"
+		><span>1</span></div>
+		<div 
+			v-if="currentPage > 5" 
+			title="向前 5 页" 
+			class="vc-page__item is-jump"
+			@click="handleFastPre"
+		>...</div>
+		<div 
+			v-if="currentPage === 5" 
+			:title="currentPage - 3"
+			class="vc-page__item" 
+			@click="handleChangePage(currentPage - 3)"
+		><span>{{ currentPage - 3 }}</span></div>
+
+		<div 
+			v-if="currentPage - 2 > 1" 
+			:title="currentPage - 2"
+			class="vc-page__item" 
+			@click="handleChangePage(currentPage - 2)"
+		><span>{{ currentPage - 2 }}</span></div>
+		<div 
+			v-if="currentPage - 1 > 1" 
+			:title="currentPage - 1"
+			class="vc-page__item" 
+			@click="handleChangePage(currentPage - 1)"
+		><span>{{ currentPage - 1 }}</span></div>
+	
+		<!-- 当前页 -->
+		<div 
+			v-if="currentPage != 1 && currentPage != allPages" 
+			:title="currentPage"
+			class="vc-page__item is-active"
+		><span>{{ currentPage }}</span></div>
+		<!-- 分割线 -->
+
+		<div 
+			v-if="currentPage + 1 < allPages" 
+			:title="currentPage + 1"
+			class="vc-page__item" 
+			@click="handleChangePage(currentPage + 1)"
+		><span>{{ currentPage + 1 }}</span></div>
+		<div 
+			v-if="currentPage + 2 < allPages" 
+			:title="currentPage + 2"
+			class="vc-page__item" 
+			@click="handleChangePage(currentPage + 2)"
+		><span>{{ currentPage + 2 }}</span></div>
+		<div 
+			v-if="allPages - currentPage === 4" 
+			:title="currentPage + 3"
+			class="vc-page__item" 
+			@click="handleChangePage(currentPage + 3)"
+		><span>{{ currentPage + 3 }}</span></div>
+		<div 
+			v-if="allPages - currentPage >= 5" 
+			title="向后 5 页"
+			class="vc-page__item is-jump"
+			@click="handleFastNext"
+		>...</div>
+		<div 
+			v-if="allPages > 1" 
+			:title="allPages" 
+			:class="{'is-active': currentPage == allPages }"
+			class="vc-page__item"
+			@click="handleChangePage(allPages)"
+		><span>{{ allPages }}</span></div>
+		<div
+			:class="{'is-disabled': currentPage == allPages }"
+			title="next"
+			class="vc-page__item is-icon"
+			@click="handleNext"
+		>
+			<vc-icon type="right" />
+		</div>
+
+		<div v-if="showSizer || showElevator" class="vc-page__size">
+			<div v-if="showSizer">
+				<vc-select
+					v-model="currentPageSize"
+					:placement="placement" 
+					:portal="portal" 
+					:extra="`${currentPageSize} 条/页`"
+					style="width: 90px; margin-right: 10px;"
+					@change="handleChangePageSize"
+				>
+					<vc-option 
+						v-for="item in pageSizeOpts" 
+						:key="item" 
+						:value="item"
+						:label="item"
+					>{{ item }} 条/页</vc-option>
+				</vc-select>
+			</div>
+			<div v-if="showElevator">
+				<span>跳至</span>
+				<vc-input-number
+					v-model="inputPage"
+					:spellcheck="false"
+					:step="0"
+					:min="1"
+					:max="allPages"
+					style="width: 50px; margin-right: 10px;"
+					autocomplete="off"
+					@enter="handleEnter"
+				/>
+				<span>页</span>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
-import Page from 'iview/src/components/page';
+import Icon from '../icon/index';
+import Select from '../select/index';
+import Option from '../option/index';
+import Input from '../input/index';
 
 export default {
-	name: "vc-page",
+	name: 'vc-page',
 	components: {
-		'i-page': Page
+		'vc-icon': Icon,
+		'vc-input-number': Input.Number,
+		'vc-select': Select,
+		'vc-option': Option,
 	},
 	props: {
-		...Page.props,
+		current: {
+			type: Number,
+			default: 1
+		},
 		count: {
-			type: String | Number, // iview为total
+			type: Number,
 			default: 0
 		},
-		showCount: {
-			type: Boolean, // iview为show-total
-			default: true
+		pageSize: {
+			type: Number,
+			default: 10
+		},
+		pageSizeOpts: {
+			type: Array,
+			default: () => ([10, 20, 30, 40])
+		},
+		placement: {
+			type: String,
+			default: 'bottom'
 		},
 		portal: {
 			type: Boolean,
 			default: true
 		},
+		showCount: {
+			type: Boolean,
+			default: false
+		},
+		showElevator: {
+			type: Boolean,
+			default: false
+		},
+		showSizer: {
+			type: Boolean,
+			default: false
+		},
 	},
 	data() {
 		return {
+			currentPage: this.current,
+			inputPage: this.current,
+			currentPageSize: this.pageSize,
 		};
 	},
 	computed: {
-		
+		allPages() {
+			const allPage = Math.ceil(this.count / this.currentPageSize);
+			return (allPage === 0) ? 1 : allPage;
+		}
+	},
+	watch: {
+		count(val) {
+			let maxPage = Math.ceil(val / this.currentPageSize);
+			if (maxPage < this.currentPage) {
+				this.currentPage = (maxPage === 0 ? 1 : maxPage);
+			}
+		},
+		current(val) {
+			this.currentPage = val;
+		},
+		pageSize(val) {
+			this.currentPageSize = val;
+		}
+	},
+	created() {
+		/**
+		 * 外部可掉用
+		 */
+		this.prev = ::this.handlePrev;
+		this.next = ::this.handleNext;
+		this.resetPage = ::this.handleChangePage;
 	},
 	methods: {
+		handleChangePage(page) {
+			if (this.currentPage != page) {
+				this.currentPage = page;
+				this.$emit('update:current', page);
+				this.$emit('on-change', page);
+			}
+		},
+		handlePrev() {
+			const current = this.currentPage;
+			if (current <= 1) {
+				return false;
+			}
+			this.resetPage(current - 1);
+		},
+		handleNext() {
+			const current = this.currentPage;
+			if (current >= this.allPages) {
+				return false;
+			}
+			this.resetPage(current + 1);
+		},
+		handleFastPre() {
+			const page = this.currentPage - 5;
+			if (page > 0) {
+				this.resetPage(page);
+			} else {
+				this.resetPage(1);
+			}
+		},
+		handleFastNext() {
+			const page = this.currentPage + 5;
+			if (page > this.allPages) {
+				this.resetPage(this.allPages);
+			} else {
+				this.resetPage(page);
+			}
+		},
+		handleChangePageSize() {
+			
+		},
+		handleEnter(v) {
+			this.resetPage(Number(this.inputPage));
+		}
 	}
 };
 </script>
-<style></style>
+<style lang="scss">
+@import '../style/index.scss';
+$block: vc-page;
+$size: 28px;
+
+@include block($block) {
+	display: flex;
+	align-items: center;
+	@include commonClearfix();
+	@include element(item) {
+		min-width: $size;
+		height: $size;
+		line-height: $size;
+		margin-right: 4px;
+		text-align: center;
+		background-color: #fff;
+		user-select: none;
+		cursor: pointer;
+		font-family: "Arial";
+		font-weight: 500;
+		border-radius: 4px;
+		@include commonBorder1PX('', #dcdee2);
+		span {
+			font-family: "Monospaced Number";
+			color: #515a6e;
+		}
+		@include when(jump) {
+			&:before, &:after {
+				border-color: #0000!important;
+			};
+		}
+		@include when(active) {
+			span {
+				color: #2d8cf0 !important;	
+			}
+			&:before, &:after {
+				border-color: #2d8cf0;
+			};
+		}
+		&:hover {
+			span {
+				color: #2d8cf0 !important;	
+			}
+			&:before, &:after {
+				border-color: #2d8cf0;
+			};
+		}
+		@include when(disabled) {
+			cursor: not-allowed;
+			color: #ccc;
+		}
+		@include when(icon) {
+			@include commonFlexCc();
+			.vc-icon {
+				font-size: 12px;
+				transform: scale(0.8);
+			}
+			&:hover {
+				color: #2d8cf0 !important;
+			}
+			@include when(disabled) {
+				cursor: not-allowed;
+				color: #ccc;
+				&:hover {
+					color: #ccc !important;
+				}
+				&:before, &:after {
+					border-color: #ccc;
+				};
+			}
+		}
+	}
+	@include element(count) {
+		display: inline-block;
+		height: $size;
+		line-height: $size;
+		margin-right: 10px;
+	}
+	@include element(size) {
+		margin-left: 15px;
+		display: flex;
+		align-items: center;
+	}
+}
+</style>
