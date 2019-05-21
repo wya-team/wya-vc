c52587555dc1e50e6ca707768dbbd10c8c7f42ca
JDEast1029 <852778359@qq.com>
2019-05-21 14:17:35 +0800

    Update editor.vue

--- src/editor/editor.vue
+++ src/editor/editor.vue
@@ -136,14 +136,15 @@ export default {
 			});
 		},
 		initFontSize() {
+			const fontSize = ['12px', '14px', '16px', '18px', '20px', '22px', '24px'];
 			let Parchment = Quill.import('parchment');
 			let SizeClass = new Parchment.Attributor.Class('size', 'ql-size', {
 				scope: Parchment.Scope.INLINE,
-				whitelist: ['12px', '14px', '16px', '18px', '20px', '22px', '24px']
+				whitelist: fontSize
 			});
 			let SizeStyle = new Parchment.Attributor.Style('size', 'font-size', {
 				scope: Parchment.Scope.INLINE,
-				whitelist: ['12px', '14px', '16px', '18px', '20px', '22px', '24px']
+				whitelist: fontSize
 			});
 			Quill.register({
 				'attributors/class/size': SizeClass,
