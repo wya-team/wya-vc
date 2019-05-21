adcdb811aa53906ac950641d17cf4354b9dcb9a7
JDEast1029 <852778359@qq.com>
2019-05-21 22:10:46 +0800

    Update customer.js

--- src/customer/customer.js
+++ src/customer/customer.js
@@ -6,12 +6,24 @@ export default {
 		render: Function
 	},
 	render(h, ctx) {
+		 let attrs = ctx.data.attrs || {};
+		 attrs = Object.keys(attrs).reduce((pre, curKey) => {
+			 let attrKey = curKey;
+			 if (curKey.includes('-')) {
+				attrKey = curKey.split('-').reduce((pre, item, index) => {
+					pre += index === 0 ? item : item.charAt(0).toUpperCase() + item.slice(1);
+					return pre;
+				}, '');
+			 }
+			 pre[attrKey] = attrs[curKey];
+			 return pre;
+		}, {});
 		let className = ctx.data.staticClass || '';
 		className = `${(className ? `${className} ` : '')}${ctx.data.class || ''}`;
 		let style = Object.assign({}, ctx.data.staticStyle, ctx.data.style);
 		
 		const params = {
-			...ctx.data.attrs,
+			...attrs,
 			className,
 			style,
 		};
