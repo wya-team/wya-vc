38884337054d6c530b3da343521ea1a8206979b8
JDEast1029 <852778359@qq.com>
2019-06-03 20:58:54 +0800

    Update manager.js

--- src/icon/manager.js
+++ src/icon/manager.js
@@ -9,7 +9,7 @@ let pathReg = /<path.*?<\/path>/gi;
 let dReg = /.*d="([^"]+).*/g;
 let fillReg = /.*fill="([^"]+).*/g;
 
-let basicUrl = '//at.alicdn.com/t/font_1119857_s9p062k318.js';
+let basicUrl = '//at.alicdn.com/t/font_1119857_8actm31jiha.js';
 let prefix = '@wya/vc-icon:';
 
 class Manager extends VcBasic {
