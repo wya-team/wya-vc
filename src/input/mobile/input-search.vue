787a7371d81ad3db7ea09cbf67cc2b576bc7bb69
mario <18758191423@163.com>
2019-07-18 17:54:13 +0800

    update m-input-search touchstart to touchend

--- src/input/mobile/input-search.vue
+++ src/input/mobile/input-search.vue
@@ -20,7 +20,7 @@
 		<div 
 			v-if="$refs.input && $refs.input.isFocus" 
 			class="vcm-input-search__btn"
-			@touchstart="handleCancel"
+			@touchend="handleCancel"
 		>
 			{{ cancelTxt }}
 		</div>
