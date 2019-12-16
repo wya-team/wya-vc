94f13755283b50873109dacc8ea26a1b97ee4260
Mr.Z <zrd0921@qq.com>
2019-12-16 11:43:11 +0800

    feat: text component

---
+++ src/text/examples/basic.vue
@@ -0,0 +1,150 @@
+<!-- 仅展示最基本的用法 -->
+<template>
+	<div style="overflow: auto; width: 100%; padding: 20px">
+		<vc-slider v-model="width" :max="maxWidth" :min="0" style="width: 100px;" />
+		<div :style="{ width: width + 'px' }">
+			<h3>line: 0</h3>
+			<vc-text :value="text14" :line="0" />
+			<h3>line: 1</h3>
+			<vc-text :value="text16" :line="1" />
+			<h3>line: 2</h3>
+			<vc-text :value="text18" :line="2" />
+			<h3>line: 3</h3>
+			<vc-text :value="text30" :line="3" />
+		</div>
+		<vc-text :line="3">
+			{{ text30 }}
+		</vc-text>
+		<table :style="{ width: width + 'px' }" class="table">
+			<thead>
+				<tr>
+					<th>line: 0</th>
+					<th>line: 1</th>
+					<th>line: 2</th>
+					<th>line: 3</th>
+				</tr>
+			</thead>
+			<colgroup>
+				<col width="100">
+				<col width="100">
+				<col width="150">
+				<col width="200">
+			</colgroup>
+			<tbody>
+				<tr> 
+					<td><vc-text :value="text10" :line="0" /></td>
+					<td><vc-text :value="text12" :line="1" /></td>
+					<td><vc-text :value="text14" :line="2" /></td>
+					<td><vc-text :value="text30" :line="3" /></td>
+				</tr>
+				<tr>
+					<td><vc-text :value="text10" :line="0" /></td>
+					<td><vc-text :value="text12" :line="1" /></td>
+					<td><vc-text :value="text14" :line="2" /></td>
+					<td><vc-text :value="text30" :line="3" /></td>
+				</tr>
+			</tbody>
+		</table>
+
+		<vc-table :data-source="dataSource" :style="{ width: width + 'px' }">
+			<vc-table-item>
+				<vc-table-column label="日期">
+					<template #default>
+						<vc-text :value="text10" :line="0" />
+					</template>
+				</vc-table-column>
+				<vc-table-column label="姓名">
+					<template #default>
+						<vc-text :value="text12" :line="1" />
+					</template>
+				</vc-table-column>
+				<vc-table-column label="地址">
+					<template #default>
+						<vc-text :value="text14" :line="2" />
+					</template>
+				</vc-table-column>
+				<vc-table-column label="地址2">
+					<template #default>
+						<vc-text :value="text30" :line="3" />
+					</template>
+				</vc-table-column>
+			</vc-table-item>
+		</vc-table>
+	</div>
+</template>
+<script>
+import Text from '..';
+import Slider from '../../slider';
+import Table from '../../table';
+
+export default {
+	name: "vc-tpl-basic",
+	components: {
+		'vc-text': Text,
+		'vc-slider': Slider,
+		'vc-table': Table,
+		'vc-table-column': Table.Column,
+		'vc-table-item': Table.Item,
+	},
+	data() {
+		let text = 'A2，C,我E,';
+		return {
+			dataSource: Array.from({ length: 4 }).map(() => ({})),
+			width: window.innerWidth - 40,
+			maxWidth: window.innerWidth - 40,
+			text,
+			text10: text.repeat(10),
+			text12: text.repeat(12),
+			text14: text.repeat(14),
+			text16: text.repeat(16),
+			text18: text.repeat(18) + 'REPEAT_END_18',
+			text30: text.repeat(30) + 'REPEAT_END_30',
+		};
+	},
+	computed: {
+		
+	},
+	methods: {
+	}
+};
+</script>
+
+<style lang="scss">
+.table {
+	overflow-x: auto;
+	background: #fff;
+	position: relative;
+	padding: 20px;
+	border-collapse: collapse;
+	width: 100%;
+	text-align: left;
+	-webkit-border-radius: 4px;
+	-moz-border-radius: 4px;
+	border-radius: 4px
+}
+
+.table thead th {
+	background: #f0f2f7;
+	font-size: 14px;
+	font-weight: 400;
+	vertical-align: middle;
+	white-space: nowrap;
+	padding: 9px 15px
+}
+
+.table tbody td {
+	line-height: 22px;
+	padding: 9px 15px;
+	border-bottom: 1px solid #ebebeb !important
+}
+
+.table tbody td:after {
+	content: "";
+	display: block;
+	clear: both;
+	height: 0;
+	font-size: 0;
+	visibility: hidden
+}
+
+</style>
---
+++ src/text/text.vue
@@ -0,0 +1,123 @@
+<template>
+	<component :is="tag" class="vc-text">
+		<span v-if="showText">
+			{{ endIndex > 0 ? `${value.slice(0, endIndex)}${suffix}` : value }}
+		</span>
+		<div
+			ref="hide"
+			:style="{
+				minHeight: `${lineHeight * line}px`,
+				textIndent: `${indent}px`
+			}"
+			class="vc-text__hide"
+		/>
+	</component>
+</template>
+<script>
+import { $ } from '@wya/utils';
+import { Resize } from '../utils/resize';
+
+export default {
+	name: 'vc-text',
+	components: { },
+	props: {
+		tag: {
+			type: String,
+			default: 'div'
+		},
+		value: {
+			type: String,
+			default: ''
+		},
+		line: {
+			type: Number,
+			default: 0
+		},
+		indent: {
+			type: Number,
+			default: 0
+		},
+		suffix: {
+			type: String,
+			default: '...'
+		}
+	},
+	data() {
+		return {
+			lineHeight: 0,
+			showText: false,
+			endIndex: 0,
+		};
+	},
+	watch: {
+		'value': 'calcPosition',
+		'indent': 'calcPosition',
+		'line': 'calcPosition',
+	},
+	created() {
+		this.timer = null;
+	},
+	mounted() {
+		this.lineHeight = parseInt($(this.$el).getStyle('line-height'), 10);
+
+		setTimeout(this.calcPosition, 0);
+
+		Resize.on(this.$el, this.handleResize);
+	},
+	beforeDestroy() {
+		Resize.off(this.$el, this.handleResize);
+		this.timer && clearTimeout(this.timer);
+	},
+	methods: {
+
+		handleResize(immediate) {
+			this.timer && clearTimeout(this.timer);
+			this.timer = setTimeout(this.calcPosition, 50);
+		},
+
+		calcPosition() {
+			const { suffix } = this;
+			this.endIndex = 0;
+			if (this.line === 0) {
+				this.showText = true;
+				return;
+			}
+			let el = this.$refs.hide;
+			el.innerText = suffix;
+
+			// 操作DOM
+			this.value.split('').forEach((item, i) => {
+				// 后缀必须放入后面计算，前面会造成问题
+				let old = el.innerText;
+				old = old.substring(0, old.length - suffix.length);
+
+				el.innerText = old + item + suffix;
+				if (el.clientHeight > this.lineHeight * this.line && this.endIndex === 0) {
+					this.endIndex = i;
+				}
+			});
+			this.showText = true;
+		}
+	}
+};
+</script>
+<style lang="scss">
+.vc-text {
+	position: relative;
+	display: inline-block;
+	word-break: break-all;
+	width: 100%;
+	&__hide {
+		position: absolute;
+		word-break: break-all;
+		width: 100%;
+		overflow: auto;
+		opacity: 0;
+		z-index: -1;
+	}
+	span {
+		word-break: break-all;
+		display: block;
+	}
+}
+</style>
