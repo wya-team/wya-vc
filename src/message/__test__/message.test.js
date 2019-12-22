e4543bd86615f84b2e291a04c033b212b6aa9878
陈敏航 <18758191423@163.com>
2019-12-22 10:39:08 +0800

    add message.test

--- src/message/__test__/message.test.js
+++ src/message/__test__/message.test.js
@@ -1,8 +1,30 @@
-import { createVue } from '@tests/helper';
-import Message from '..';
+import { createVue, createComponent, wait, triggerEvent } from '@tests/helper';
+import Message from '../index';
+import MMessage from '../index.m';
 
 describe('Message', () => {
 	it('basic', () => {
 		expect(!!Message).to.equal(true);
+	});
+	it('message open', (done) => {
+		Message.success({
+			content: '测试',
+			duration: 2
+		});
+		const messageEle = document.querySelector('.vc-message__content'); 
+		const messageWrapper = document.querySelector('.vc-message__wrapper');
+		expect(messageEle.textContent).to.equal('测试');
+		expect(messageWrapper).to.exist;
+		setTimeout(() => {
+			expect(messageWrapper.style.display).to.equal('none');
+			done();
+		}, 3000);
+	});
+	it('close message', () => {
+		Message.loading({
+			content: '啦啦啦，啦啦啦，我是买包的小行家',
+			closable: true
+		});
+		triggerEvent(document.querySelector(''));
 	});
 });
