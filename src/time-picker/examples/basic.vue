d8427db0324334a9b739222a7b6c458d491e2fe5
蒋东 <852778359@qq.com>
2019-12-23 20:28:44 +0800

    update: 完善time-picker表单验证用例

--- src/time-picker/examples/basic.vue
+++ src/time-picker/examples/basic.vue
@@ -41,15 +41,34 @@
 			placeholder="Select time" 
 			style="width: 168px"
 		/>
+		<h2>Form表单校验</h2>
+		<vc-form
+			ref="form"
+			:model="formValidate" 
+			:rules="ruleValidate" 
+		>
+			<vc-form-item prop="time">
+				<vc-time-picker 
+					v-model="formValidate.time"
+					placeholder="Select time" 
+					style="width: 168px"
+				/>
+			</vc-form-item>
+			<div @click="handleSubmit">提交</div>
+		</vc-form >
 	</div>
 </template>
 <script>
+import Message from '../../message';
+import Form from '../../form';
 import TimePicker from '..';
 
 export default {
 	name: "vc-time-picker-basic",
 	components: {
-		'vc-time-picker': TimePicker
+		'vc-time-picker': TimePicker,
+		'vc-form': Form,
+		'vc-form-item': Form.Item,
 	},
 	data() {
 		return {
@@ -58,12 +77,29 @@ export default {
 			time1: '',
 			time2: '',
 			time3: '',
+			formValidate: {
+				time: ''
+			},
+			ruleValidate: {
+				time: [
+					{ required: true, message: '请选择时间', trigger: 'change' }
+				],
+			}
 		};
 	},
 	computed: {
 		
 	},
 	methods: {
+		handleSubmit(name) {
+			this.$refs.form.validate((valid) => {
+				if (valid) {
+					Message.success('Success!');
+				} else {
+					Message.error('Fail!');
+				}
+			});
+		},
 	}
 };
 </script>
