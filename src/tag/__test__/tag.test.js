569457407854d94627097ea4191ebb0cbb8cf96d
涂志良 <tzl@weiyian.com>
2020-01-02 14:12:40 +0800

    test: switch, tag, card,page

--- src/tag/__test__/tag.test.js
+++ src/tag/__test__/tag.test.js
@@ -1,8 +1,84 @@
-import { createVue } from '@tests/helper';
-import Tag from '..';
+import { createVue, createComponent } from '@tests/helper';
+import Tag from '../index';
 
 describe('Tag', () => {
 	it('basic', () => {
-		expect(!!Tag).to.equal(true);
+		expect(typeof Tag).to.equal('object');
+
+		const vm = createComponent(Tag, {});
+		expect(typeof vm).to.equal('object');
+	});
+
+	it('closable', () => {
+		let count = 0;
+		let vm = createVue({
+			template: `
+				<vc-tag closable @close="handleClose"></vc-tag>
+			`,
+			components: {
+				'vc-tag': Tag
+			},
+			data() {
+				return {
+
+				};
+			},
+			methods: {
+				handleClose() {
+					count++;
+				}
+			}
+		});
+
+		vm.$el.querySelector('.vc-tag__close').click();
+		expect(count).to.equal(1);
+	});
+	it('check', () => {
+		let count = 0;
+		let vm = createVue({
+			template: `
+				<vc-tag checkable @click="handleClick"></vc-tag>
+			`,
+			components: {
+				'vc-tag': Tag
+			},
+			data() {
+				return {
+
+				};
+			},
+			methods: {
+				handleClose() {
+					count++;
+				}
+			}
+		});
+
+		vm.$el.click();
+		expect(count).to.equal(1);
+	});
+	it('checkable', () => {
+		let count = 0;
+		let vm = createVue({
+			template: `
+				<vc-tag :checkable="false" @click="handleClick"></vc-tag>
+			`,
+			components: {
+				'vc-tag': Tag
+			},
+			data() {
+				return {
+
+				};
+			},
+			methods: {
+				handleClose() {
+					count++;
+				}
+			}
+		});
+
+		vm.$el.click();
+		expect(count).to.equal(0);
 	});
 });
