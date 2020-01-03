3f728c3d8d9f332030b0902dc9a1a935f184ee35
ScoutYin <yinbin_web@163.com>
2020-01-03 09:56:10 +0800

    test: popconfirm

--- src/popconfirm/__test__/popconfirm.test.js
+++ src/popconfirm/__test__/popconfirm.test.js
@@ -1,5 +1,4 @@
 import { createVue, destroyVM } from '@tests/helper';
-import { DropdownMenu } from 'iview';
 import Popconfirm from '..';
 
 describe('Popconfirm', () => {
@@ -12,7 +11,9 @@ describe('Popconfirm', () => {
 		expect(!!Popconfirm).to.equal(true);
 	});
 
-	const createVM = () => {
+	const createVM = (options = {}) => {
+		const { promise = true } = options;
+
 		const contentRender = (h) => {
 			return h(
 				'div',
@@ -27,7 +28,8 @@ describe('Popconfirm', () => {
 				<vc-popconfirm 
 					v-model="visible" 
 					ref="popconfirm" 
-					:render-content="contentRender"
+					:renderContent="contentRender"
+					@ok="handleOk"
 				>
 					<div class="_trigger">trigger</div>
 				</vc-popconfirm>
@@ -37,48 +39,31 @@ describe('Popconfirm', () => {
 			},
 			data() {
 				return {
-					visible: false
+					visible: false,
+					flag: false,
+					promise
 				};
 			},
 			methods: {
-				contentRender
+				contentRender,
+				handleOk(e, cb) {
+					if (this.promise) {
+						return new Promise((resolve, reject) => {
+							if (this.flag) {
+								cb();
+								resolve();
+							} else {
+								reject();
+							}
+						});
+					}
+				}
 			}
 		});
 	};
 
 	it('cancel', done => {
-		// vm = createVM();
-		const contentRender = (h) => {
-			return h(
-				'div',
-				{
-					class: '_content'
-				},
-				'This is content'
-			);
-		};
-		vm = createVue({
-			template: `
-				<vc-popconfirm 
-					v-model="visible" 
-					ref="popconfirm" 
-					:render-content="contentRender"
-				>
-					<div class="_trigger">trigger</div>
-				</vc-popconfirm>
-			`,
-			components: {
-				'vc-popconfirm': Popconfirm
-			},
-			data() {
-				return {
-					visible: false
-				};
-			},
-			methods: {
-				contentRender
-			}
-		});
+		vm = createVM();
 
 		const popEl = vm.$refs.popconfirm.$el;
 
@@ -88,81 +73,90 @@ describe('Popconfirm', () => {
 
 		setTimeout(() => {
 			const content = document.body.querySelector('._content');
+			expect(vm.visible).to.be.true;
 			expect(content).to.be.exist;
+
 			const footerBtns = document.body.querySelectorAll('.vc-popconfirm__footer .vc-btn');
 			footerBtns[0].click();
 		}, 500);
 
 		setTimeout(() => {
 			const content = document.body.querySelector('._content');
+			expect(vm.visible).to.be.false;
 			expect(content).to.be.null;
 			done();
-		}, 2000);
+		}, 1000);
 
 	});
 
-	it('promise ok', done => {
-		vm = createVue({
-			template: `
-				<vc-popconfirm 
-					v-model="visible" 
-					ref="popconfirm" 
-					@ok="handleOk"
-				>
-					<div class="_trigger">trigger</div>
-					<template>
-						<div class="_content">This is content.</div>
-					</template>
-				</vc-popconfirm>
-			`,
-			components: {
-				'vc-popconfirm': Popconfirm
-			},
-			data() {
-				return {
-					visible: false,
-					flag: false
-				};
-			},
-			methods: {
-				handleOk(e, cb) {
-					return new Promise((resolve, reject) => {
-						if (this.flag) {
-							cb();
-							resolve();
-						} else {
-							reject();
-						}
-					});
-				}
-			}
-		});
+	it('sync ok', done => {
+
+		vm = createVM({ promise: false });
 
 		const instance = vm.$refs.popconfirm;
 		instance.$el.querySelector('._trigger').click();
 
 		expect(vm.visible).to.be.true;
 
+		let actionBtns;
+		let content;
+
 		setTimeout(() => {
-			const content = document.body.querySelector('._content');
+			content = document.body.querySelector('._content');
+			expect(vm.visible).to.be.true;
 			expect(content).to.be.exist;
-			const footerBtns = document.body.querySelectorAll('.vc-popconfirm__footer .vc-btn');
-			footerBtns[1].click();
+
+			actionBtns = document.body.querySelectorAll('.vc-popconfirm__footer .vc-btn');
+			actionBtns[1].click();
 		}, 500);
 
 		setTimeout(() => {
-			const content = document.body.querySelector('._content');
-			expect(content).to.be.exist;
-			vm.flag = true;
-			const footerBtns = document.body.querySelectorAll('.vc-popconfirm__footer .vc-btn');
-			footerBtns[1].click();
-		}, 1000);
-
-		setTimeout(() => {
-			const content = document.body.querySelector('._content');
+			content = document.body.querySelector('._content');
+			expect(vm.visible).to.be.false;
 			expect(content).to.be.null;
 			done();
-		}, 5000);
+		}, 1000);
+	});
 
+	it('promise ok', done => {
+		vm = createVM();
+
+		const instance = vm.$refs.popconfirm;
+		instance.$el.querySelector('._trigger').click();
+
+		expect(vm.visible).to.be.true;
+
+		let actionBtns;
+		let content;
+
+		// 期望页面上存在popover
+		setTimeout(() => {
+			content = document.body.querySelector('._content');
+			expect(vm.visible).to.be.true;
+			expect(content).to.be.exist;
+
+			actionBtns = document.body.querySelectorAll('.vc-popconfirm__footer .vc-btn');
+			// 点击‘确定’，此时期望reject
+			actionBtns[1].click();
+		}, 500);
+
+		// 期望页面上的popover未被关闭
+		setTimeout(() => {
+			content = document.body.querySelector('._content');
+			expect(vm.visible).to.be.true;
+			expect(content).to.be.exist;
+
+			// 再次点击‘确定’，此时期望resolve
+			vm.flag = true;
+			actionBtns[1].click();
+		}, 1000);
+
+		// 期望页面上的popover已被关闭，dom中不存在
+		setTimeout(() => {
+			content = document.body.querySelector('._content');
+			expect(vm.visible).to.be.false;
+			expect(content).to.be.null;
+			done();
+		}, 1500);
 	});
 });
