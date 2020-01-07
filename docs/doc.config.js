51395932699f23425d3dd8426f253d44b9877244
Mr.Z <zrd0921@qq.com>
2020-01-07 14:34:35 +0800

    clean

--- docs/doc.config.js
+++ docs/doc.config.js
@@ -24,35 +24,32 @@ module.exports = {
 			footer: 'default',
 			extra: null,
 		},
-		'/api/:name': {
-			default: 'default',
-			sidebar,
-			header: './components/header.vue',
-			footer: './components/footer.vue',
-			extra: null,
-		},
 		'*': (to) => {
 			return '/zh-CN/index';
 		}
 	},
 	locales: {
-		'zh-CN': '简体中文',
-		'en-US': 'English'
+		'zh-CN': '简体中文'
 	},
 	webpackConfig: {
 		output: {
 			path: path.resolve(__dirname, '../site/'),
 			publicPath: ENV_IS_DEV
 				? '/'
-				: '/wya-doc/site/'
+				: '/wya-vc/site/'
 		},
 		devServer: {},
 	},
 	runtime: { 
 		define: {
 			__DOC_SITE_DIR__: `'${ENV_IS_DEV ? '/' : '/wya-vc/site/'}'`,
-			__DOC_MD_DIR__: `'${ENV_IS_DEV ? '/docs/' : '/wya-vc/docs/'}'`,
-			// __DOC_MD_DIR__: (lang, name) => { return lang + name; },
+			__DOC_MD_DIR__: ENV_IS_DEV 
+				? () => (lang, name) => { 
+					return `/src/${name}/README${lang != 'zh-CN' ? `_${lang}` : ''}.md`; 
+				}
+				: (lang, name) => { 
+					return `/wya-vc/src/${name}/README${lang != 'zh-CN' ? `_${lang}` : ''}.md`; 
+				},
 			__DOC_VERSION__: "'1.0.0'",
 		}
 	}
