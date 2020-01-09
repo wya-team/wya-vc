407fff1bdf1736541e27f842bf883ae985ced9f6
蒋东 <852778359@qq.com>
2020-01-09 19:26:20 +0800

    update: font-size样式动态创建

--- src/editor/toolbar.js
+++ src/editor/toolbar.js
@@ -1,3 +1,4 @@
+import { Load } from '@wya/utils';
 import defaultOptions from './default-options';
 
 const toolMap = {
@@ -86,7 +87,22 @@ export default {
 					} 
 					if (value instanceof Array) {
 						let options = value || toolMap[key];
-						key === 'size' && (this.fontSize = value);
+
+						if (key === 'size') {
+							this.fontSize = value;
+							// 动态生成font-size的css，插入head
+							let code = value.reduce((pre, fontSize) => {
+								pre += `.ql-snow .ql-size.ql-picker .ql-picker-item[data-value="${fontSize}"]:before,
+										.ql-snow .ql-size.ql-picker .ql-picker-label[data-value="${fontSize}"]:before {
+											content: "${fontSize}"
+										}`;
+								pre += `.ql-editor .ql-size-${fontSize} {
+											font-size: ${fontSize};
+										}`;
+								return pre;
+							}, '');
+							Load.cssCode(code, { id: 'vc-editor-fontsize' });
+						}
 						
 						return (
 							<select class={`ql-${key}`}>
