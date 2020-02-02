f9df6fdaf792a6562f7e0124f0ee392c87031ce9
Mr.Z <zrd0921@qq.com>
2020-02-02 16:53:05 +0800

    fix: test errors

--- src/message/__test__/message.test.js
+++ src/message/__test__/message.test.js
@@ -1,4 +1,4 @@
-import { createVue, createComponent, wait, triggerEvent } from '@tests/helper';
+import { createVue, createComponent, wait, triggerEvent, createEl, destroyEl, destroyVM } from '@tests/helper';
 import Message from '../index';
 import MMessage from '../index.m';
 
@@ -6,22 +6,22 @@ describe('Message', () => {
 	it('basic', () => {
 		expect(!!Message).to.equal(true);
 	});
-	it('message open', (done) => {
+	it('message open', async () => {
 		let vm = Message.success({
 			content: '测试',
 			duration: 2
 		});
 		const messageEle = vm.$el.querySelector('.vc-message__content'); 
-		const messageWrapper = document.querySelector('.vc-message__wrapper');
+		const messageWrapper = vm.$el.querySelector('.vc-message__wrapper');
 		expect(messageEle.textContent).to.equal('测试');
 		expect(messageWrapper).to.exist;
-		setTimeout(() => {
-			expect(messageWrapper.style.display).to.equal('none');
-			Message.destroy();
-			done();
-		}, 3000);
+
+		await wait(2.1);
+		expect(messageWrapper.style.display).to.equal('none');
+
+		destroyVM(vm);
 	});
-	it('not close message', (done) => {
+	it('not close message', async () => {
 		let vm = Message.info({
 			content: '啦啦啦，啦啦啦，我是买包的小行家',
 			duration: 0,
@@ -30,56 +30,60 @@ describe('Message', () => {
 		});
 		const messageWrapper = vm.$el.querySelector('.vc-message__wrapper');
 		expect(messageWrapper).to.exist;
-		setTimeout(() => {
-			triggerEvent(vm.$el.querySelector('.vc-message__mask'), 'click');
-			expect(messageWrapper.style.display).to.not.equal('none');
-			Message.destroy();
-			done();
-		}, 1500);
+
+		await wait(1.5);
+		triggerEvent(vm.$el.querySelector('.vc-message__mask'), 'click');
+		expect(messageWrapper.style.display).to.not.equal('none');
+
+		destroyVM(vm);
 	});
-	it('mask 是否开启', (done) => {
+	it('mask 是否开启', async () => {
 		let vm = Message.error({
 			content: '啦啦啦，啦啦啦，我是买包的小行家',
 			mask: false,
 			duration: 0
 		});
+
+		await wait(1.5);
 		const messageMask = vm.$el.querySelector('.vc-message__mask');
-		setTimeout(() => {
-			expect(messageMask).to.not.exist;
-			Message.destroy();
-			done();
-		}, 1500);
+		expect(messageMask).to.not.exist;
+
+		destroyVM(vm);
 	});
-	it('多个message', (done) => {
-		Message.error({
+	it('多个message', async () => {
+		let el = createEl();
+		let vm1 = Message.error({
+			el,
 			content: '啦啦啦，啦啦啦，我是买包的小行家',
 			duration: 0
 		});
-		Message.warning({
+		let vm2 = Message.warning({
+			el,
 			content: '啦啦啦，啦啦啦，我是买包的小行家',
 			duration: 0
 		});
-		setTimeout(() => {
-			expect(document.querySelectorAll('.vc-message').length).to.equal(2);
-			Message.destroy();
-			done();
-		}, 1500);
+
+		await wait(1.5);
+		expect(el.querySelectorAll('.vc-message').length).to.equal(2);
+		destroyVM(vm1);
+		destroyVM(vm2);
+		destroyEl(el);
 	});
-	it('close message', (done) => {
+	it('close message', async () => {
 		let vm = Message.loading({
 			content: '啦啦啦，啦啦啦，我是买包的小行家',
 			mask: true,
 			duration: 0
 		});
-		setTimeout(() => {
-			const messageWrapper = vm.$el.querySelector('.vc-message__wrapper');
-			expect(messageWrapper).to.exist;
-			expect(messageWrapper.style.display).to.not.equal('none');
-			Message.destroy();
-			done();
-		}, 1500);
+
+		const messageWrapper = vm.$el.querySelector('.vc-message__wrapper');
+
+		await wait(1.5);
+		expect(messageWrapper).to.exist;
+		expect(messageWrapper.style.display).to.not.equal('none');
+		destroyVM(vm);
 	});
-	it('render 验证', (done) => {
+	it('render 验证', async () => {
 		let vm = Message.info({
 			content: h => {
 				return h('span', [
@@ -94,15 +98,14 @@ describe('Message', () => {
 		});
 		const messageWrapper = vm.$el.querySelector('.vc-message__wrapper');
 		const messageRender = vm.$el.querySelectorAll('a');
-		setTimeout(() => {
-			expect(messageRender.length).to.equal(1);
-			expect(vm.$el.querySelector('.vc-icon')).to.exist;
-			triggerEvent(vm.$el.querySelectorAll('.vc-icon')[1], 'click');
-			setTimeout(() => {
-				expect(messageWrapper.style.display).to.equal('none');
-				Message.destroy();
-				done();
-			}, 2000);
-		}, 1500);
+
+		await wait(1.5);
+		expect(messageRender.length).to.equal(1);
+		expect(vm.$el.querySelector('.vc-icon')).to.exist;
+		triggerEvent(vm.$el.querySelectorAll('.vc-icon')[1], 'click');
+
+		await wait(2.1);
+		expect(messageWrapper.style.display).to.equal('none');
+		destroyVM(vm);
 	});
 });
--- src/progress/__test__/progress.test.js
+++ src/progress/__test__/progress.test.js
@@ -1,4 +1,4 @@
-import { createVue, createComponent, destroyVM } from '@tests/helper';
+import { createVue, createComponent, destroyVM, wait } from '@tests/helper';
 import Progress from '../index';
 import MProgress from '../index.m';
 
@@ -11,20 +11,20 @@ describe('Progress', () => {
 		expect(!!Progress).to.equal(true);
 		expect(!!MProgress).to.equal(true);
 	});
-	it('type 为line的进度条', (done) => {
-		vm = createVue({
+	it('type 为line的进度条', async () => {
+		let vm = createVue({
 			template: `
 				<div>
 					<vc-progress
 						ref="errorLine"
 						:percent="50"
 						status="error"
-						:strokeWidth="7"
+						:stroke-width="7"
 					/>
 					<vc-progress
 						ref="successLine"
 						:percent="100"
-						:strokeWidth="7"
+						:stroke-width="7"
 					/>
 				</div>
 			`,
@@ -34,16 +34,15 @@ describe('Progress', () => {
 		});
 		expect(vm.$refs.errorLine.$el.querySelector('.vc-progress__line')).to.exist;
 		expect(vm.$refs.errorLine.$el.querySelector('.vc-progress__percent')).to.exist;
-		setTimeout(() => {
-			expect(vm.$refs.errorLine.$el.querySelector('.vc-progress__inner').style.height).to.equal('7px');
-			expect(vm.$refs.errorLine.$el.querySelector('.vc-progress__inner').style.width).to.equal('50%');
-			expect(vm.$refs.errorLine.$el.querySelector('.vc-progress__icon').style.color).to.equal('rgb(245, 34, 45)');
-			expect(vm.$refs.successLine.$el.querySelector('.vc-progress__inner').style.width).to.equal('100%');
-			expect(vm.$refs.successLine.$el.querySelector('.vc-progress__icon').style.color).to.equal('rgb(82, 196, 26)');
-			done();
-		}, 1000);
+		await wait(1);
+		expect(vm.$refs.errorLine.$el.querySelector('.vc-progress__inner').style.height).to.equal('7px');
+		expect(vm.$refs.errorLine.$el.querySelector('.vc-progress__inner').style.width).to.equal('50%');
+		expect(vm.$refs.errorLine.$el.querySelector('.vc-progress__icon').style.color).to.equal('rgb(245, 34, 45)');
+		expect(vm.$refs.successLine.$el.querySelector('.vc-progress__inner').style.width).to.equal('100%');
+		expect(vm.$refs.successLine.$el.querySelector('.vc-progress__icon').style.color).to.equal('rgb(82, 196, 26)');
+		destroyVM(vm);
 	});
-	it('type为circle的进度条', (done) => {
+	it('type为circle的进度条', async () => {
 		let vm = createVue({
 			template: `
 					<vc-progress
@@ -52,7 +51,7 @@ describe('Progress', () => {
 						percent="50"
 						:size="100"
 						status="error"
-						:strokeWidth="7"
+						:stroke-width="7"
 						
 					/>
 			`,
@@ -61,10 +60,9 @@ describe('Progress', () => {
 			}
 		});
 		expect(vm.$el.querySelector('.vc-progress__circle')).to.exist;
-		setTimeout(() => {
-			expect(vm.$el.querySelector('.vc-progress__box').style.width).to.equal('100px');
-			destroyVM(vm);
-			done();
-		}, 1000);
+
+		await wait(1);
+		expect(vm.$el.querySelector('.vc-progress__box').style.width).to.equal('100px');
+		destroyVM(vm);
 	});
 });
