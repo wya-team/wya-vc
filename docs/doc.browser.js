62217a1422ef593550ab6ef77ac80e680ca0a8e3
Mr.Z <zrd0921@qq.com>
2020-03-30 14:02:38 +0800

    feat: support `@wya/http` with doc

--- docs/doc.browser.js
+++ docs/doc.browser.js
@@ -1,3 +1,6 @@
 import * as __UTILS__ from '@wya/utils';
+import * as __HTTP__ from '@wya/http';
 
-window.__UTILS__ = __UTILS__;
+window['@wya/http'] = __HTTP__;
+
+window['@wya/utils'] = __UTILS__;
