a050081991aed87f49d49a11fc3fb5e6960d580b
颜秉斌 <785175917@qq.com>
2020-07-24 18:05:20 +0800

    test: add icon test

--- src/icon/__test__/icon.test.js
+++ src/icon/__test__/icon.test.js
@@ -1,8 +1,57 @@
-import { createVue } from '@tests/helper';
+import { createVue, createComponent, destroyVM } from '@tests/helper';
+import { expect } from 'chai';
 import Icon from '..';
 
 describe('Icon', () => {
+	let vm;
+	const ANIMATION_TIME = 300; 
+
 	it('basic', () => {
 		expect(!!Icon).to.equal(true);
+	});
+
+	it('show icon', (done) => {
+		vm = createComponent(Icon, {
+			type: 'success'
+		});
+		setTimeout(() => {
+			expect(vm.path.length > 0).to.be.true;
+			destroyVM(vm);
+			done();
+		}, ANIMATION_TIME);
+	});
+
+	it('click icon', (done) => {
+		let result;
+		vm = createVue({
+			template: `<vc-icon type="success" @click="handleClick" />`,
+			components: {
+				'vc-icon': Icon
+			},
+			methods: {
+				handleClick(e) {
+					result = e;
+				}
+			}
+		});
+		const trigger = document.querySelector('.vc-icon');
+		trigger.click();
+		setTimeout(_ => {
+			expect(result).to.exist;
+			done();
+		}, ANIMATION_TIME);
+	});
+
+	it('not exist icon', (done) => {
+		vm = createVue({
+			template: `<vc-icon type="not-find-icon"/>`,
+			components: {
+				'vc-icon': Icon
+			}
+		});
+		setTimeout(_ => {
+			expect(vm.path === undefined).to.be.true;
+			done();
+		}, ANIMATION_TIME);
 	});
 });
