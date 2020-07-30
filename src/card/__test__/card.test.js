7051d60cbb7cf781f627fe088c417247d33a9fba
颜秉斌 <785175917@qq.com>
2020-07-30 13:57:46 +0800

    test: add card test

--- src/card/__test__/card.test.js
+++ src/card/__test__/card.test.js
@@ -1,4 +1,4 @@
-import { createComponent, destroyVM } from "@tests/helper";
+import { createComponent, destroyVM, wait } from "@tests/helper";
 import Card from '../index';
 
 describe('Card', () => {
@@ -19,6 +19,45 @@ describe('Card', () => {
 
 		expect(vm.$el.classList.contains("is-border")).to.equal(true);
 
+		destroyVM(vm);
+	});
+
+	it("shadow", () => {
+		const vm = createComponent(Card, {
+			shadow: true
+		});
+		expect(vm.$el.classList.contains("is-shadow")).to.equal(true);
+
+		destroyVM(vm);
+	});
+
+	it("padding", () => {
+		const vm = createComponent(Card, {
+			padding: 10
+		});
+		const el = vm.$el.querySelector('.vc-card__body');
+		expect(el.style.padding).to.equal('10px');
+
+		destroyVM(vm);
+	});
+
+	it("title", () => {
+		const vm = createComponent(Card, {
+			title: '标题'
+		});
+		const el = vm.$el.querySelector('.vc-card__header');
+		expect(el).to.exist;
+		expect(el.innerText).to.equal('标题');
+		destroyVM(vm);
+	});
+
+	it('icon', () => {
+		const vm = createComponent(Card, {
+			title: '标题',
+			icon: 'success'
+		});
+		const el = vm.$el.querySelector('.vc-icon');
+		expect(el).to.exist;
 		destroyVM(vm);
 	});
 });
