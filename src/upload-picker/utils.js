da3bd3803b0a51daec47da30e1d55e1bf2421bc8
蒋东 <852778359@qq.com>
2020-12-30 17:03:06 +0800

    fix: 无后缀的网络资源地址，通过获取content-type来判断类型

--- src/upload-picker/utils.js
+++ src/upload-picker/utils.js
@@ -1,3 +1,29 @@
+const getContentType = (url) => {
+	let contentType = '';
+	try {
+		let req = new XMLHttpRequest();
+		req.open('GET', url, false); // false: 同步处理
+		req.send(null);
+		contentType = req.getResponseHeader("Content-Type");
+	} catch (error) {
+		console.log('getContentType', error);
+	}
+	return contentType;
+};
+
+const getTypeByContentType = (url) => {
+	console.log('111');
+	let IMG_REGEX = /^image/ig;
+	let VIDEO_REGEX = /^video/ig;
+	let contentType = getContentType(url);
+	if (IMG_REGEX.test(contentType)) {
+		return 'image';
+	} else if (VIDEO_REGEX.test(contentType)) {
+		return 'video';
+	}
+	return 'file';
+};
+
 /**
  * 通过文件url判断文件类型  --->  image（图片） | video（视频） | file（其他文件）
  * @param {*} url 文件url或者文件名
@@ -7,5 +33,5 @@ export const recognizer = (url) => {
 	const result = url.match(reg);
 	return result && result.length
 		? /.(jpe?g|png|gif|bmp|webp)/ig.test(result[result.length - 1]) ? 'image' : 'video'
-		: 'file';
+		: getTypeByContentType(url);
 };
